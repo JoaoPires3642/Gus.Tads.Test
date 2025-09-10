b0069664994316dc1662f39ee14c0525ced37620
LucasAmorimDosSantosSansiverinato <amorimlucas056@gmail.com>
2025-09-10 17:04:38 -0300

    feat(UserModel): Cria classe UserModel para representar a entidade users

--- painel-macroeconomico/src/main/java/br/com/etl/painel_macroeconomico/model/UserModel.java
+++ painel-macroeconomico/src/main/java/br/com/etl/painel_macroeconomico/model/UserModel.java
@@ -1,4 +1,29 @@
 package br.com.etl.painel_macroeconomico.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+@Entity
+@Table(name = "Users", schema = "test")
 public class UserModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome", nullable = false, length = 100)
+    private String nome;
+
+    @Column(name = "email", nullable = false, unique = true, length = 120)
+    private String email;
+
+    @Column(name = "senha", nullable = false, length = 100)
+    private String senha;
+
+    @Column(name = "data_nascimento", nullable = false)
+    private LocalDate dataNascimento;
 }
