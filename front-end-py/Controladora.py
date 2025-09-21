a70121ee8961caa371cfc2d6ac3622c773b22866
LucasAmorimDosSantosSansiverinato <amorimlucas056@gmail.com>
2025-09-20 22:05:30 -0300

    refactor(Controladora.py): atualizar declaração de importação e otimizar lógica de navegação

--- front-end-py/Controladora.py
+++ front-end-py/Controladora.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from Dashboard_login import login_page
 from Dashboard_cadastro import register_page
-from Dashboard_graphics import dashboard_page
+import Dashboard_graphics as dg
 
 # --- Estado da sessão ---
 if "page" not in st.session_state:
@@ -21,15 +21,16 @@ def go_to_dashboard():
 
 # --- Controladora ---
 def main_controller():
-    if st.session_state["authenticated"]:
-        st.session_state["page"] = "dashboard"  # garante que o logado vá para dashboard
     
     if st.session_state["page"] == "login":
         login_page(go_to_register, go_to_dashboard)
+        
     elif st.session_state["page"] == "register":
         register_page(go_to_login, go_to_dashboard)
+        
     elif st.session_state["page"] == "dashboard":
-        dashboard_page()
+        dg.main()
+        
 
 if __name__ == "__main__":
     main_controller()
