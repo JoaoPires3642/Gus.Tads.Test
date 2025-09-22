f7dafa506362ee569dc65595c772179bc10264da
LucasAmorimDosSantosSansiverinato <amorimlucas056@gmail.com>
2025-09-22 18:09:06 -0300

    feat(RestErrorMenssage): adicionar classe RestErrorMenssage para respostas de erro estruturadas

---
+++ painel-macroeconomico/src/main/java/br/com/etl/painel_macroeconomico/exceptions/RestErrorMenssage.java
@@ -0,0 +1,43 @@
+package br.com.etl.painel_macroeconomico.exceptions;
+
+import java.security.Timestamp;
+
+import org.springframework.boot.actuate.autoconfigure.observation.ObservationProperties.Http;
+
+public class RestErrorMenssage {
+    private Http status;
+    private String message;
+    private Timestamp timestamp;
+    private String path;
+    private String error;
+
+    public RestErrorMenssage(Http status, String message, Timestamp timestamp, String path, String error) {
+        super();
+        this.status = status;
+        this.message = message;
+        this.timestamp = timestamp;
+        this.path = path;
+        this.error = error;
+    }
+
+    public Http getStatus() {
+        return status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+}
