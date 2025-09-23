2e4ad47ba0dd02a03bd03b1ab21ec9dce8fd53f2
LucasAmorimDosSantosSansiverinato <amorimlucas056@gmail.com>
2025-09-23 18:16:06 -0300

    fix(UserException): update default message to specify valid email format

--- painel-macroeconomico/src/main/java/br/com/etl/painel_macroeconomico/exceptions/UserException.java
+++ painel-macroeconomico/src/main/java/br/com/etl/painel_macroeconomico/exceptions/UserException.java
@@ -1,7 +1,6 @@
 package br.com.etl.painel_macroeconomico.exceptions;
 
-public class UserException extends RuntimeException {
-    public UserException() {super("");}
+public class UserException extends RuntimeException  {
+    public UserException() {super("E-mail inválido! O email deve conter '@gmail'.");}
     public UserException(String message) {super(message);}
-
 }
