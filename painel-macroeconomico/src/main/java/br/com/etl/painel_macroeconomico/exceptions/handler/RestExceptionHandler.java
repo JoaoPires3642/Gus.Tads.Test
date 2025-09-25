5a034f3ac629d408b3cd33430a8ef6dd719b4cb6
LucasAmorimDosSantosSansiverinato <amorimlucas056@gmail.com>
2025-09-25 14:27:46 -0300

    refactor(RestExceptionHandler):  melhora os métodos de tratamento de exceções para melhor clareza e estrutura de resposta

--- painel-macroeconomico/src/main/java/br/com/etl/painel_macroeconomico/exceptions/handler/RestExceptionHandler.java
+++ painel-macroeconomico/src/main/java/br/com/etl/painel_macroeconomico/exceptions/handler/RestExceptionHandler.java
@@ -1,7 +1,6 @@
 package br.com.etl.painel_macroeconomico.exceptions.handler;
 
 
-import br.com.etl.painel_macroeconomico.dto.ErrorMenssageDTO;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,26 +8,33 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import br.com.etl.painel_macroeconomico.dto.ErrorMenssageDTO;
 import br.com.etl.painel_macroeconomico.exceptions.UserException;
+import jakarta.servlet.http.HttpServletRequest;
 
 
 @ControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler
-    private ResponseEntity<String> handlerGenericException(Exception ex ) {
-        return ResponseEntity.badRequest().body(ex.getMessage());    
-    }
-    
     @ExceptionHandler(UserException.class)
-    private ResponseEntity<String> handlerIllegalEmailException(UserException ex) {
-        return ResponseEntity.badRequest().body(ex.getMessage());    
+    public ResponseEntity<ErrorMenssageDTO> handlerUserException(UserException ex, HttpServletRequest request) {
+
+        ErrorMenssageDTO error = new ErrorMenssageDTO(
+            HttpStatus.BAD_REQUEST, 
+            ex.getMessage(),
+            request.getRequestURI()
+            );
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);    
     }
 
-    //@ExceptionHandler(UserException.class)
-    private ErrorMenssageDTO handler(ErrorMenssageDTO ex) {
-        return new ErrorMenssageDTO(
-                ex.getStatus(), "E-mail inválido! O email deve conter '@gmail'.", ex.getTimestamp(), ex.getPath()
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorMenssageDTO> handlerGenericException(Exception ex, HttpServletRequest request){
+        ErrorMenssageDTO error = new ErrorMenssageDTO(
+            HttpStatus.INTERNAL_SERVER_ERROR, 
+        "Ocorreu um erro inesperado", 
+        request.getRequestURI()
         );
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
     }
+    
 }
