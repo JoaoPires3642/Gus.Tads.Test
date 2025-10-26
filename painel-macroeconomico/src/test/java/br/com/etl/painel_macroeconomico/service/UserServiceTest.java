7a67aa5f22eace24c6499ce06216fab723c8f912
LucasAmorimDosSantosSansiverinato <amorimlucas056@gmail.com>
2025-10-26 17:33:40 -0300

    refactor(UserServiceTest): Implementa testes parametrizados

--- painel-macroeconomico/src/test/java/br/com/etl/painel_macroeconomico/service/UserServiceTest.java
+++ painel-macroeconomico/src/test/java/br/com/etl/painel_macroeconomico/service/UserServiceTest.java
@@ -1,45 +1,100 @@
 package br.com.etl.painel_macroeconomico.service;
 
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.when;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 import java.time.LocalDate;
+import java.util.stream.Stream;
 
-import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
-
-import br.com.etl.painel_macroeconomico.model.UserModel;
-import br.com.etl.painel_macroeconomico.repository.UserRepository;
+import br.com.etl.painel_macroeconomico.exceptions.UserException;
 
 public class UserServiceTest {
 
-    @InjectMocks
-    private UserService userService;
-
-    @Mock
-    private UserRepository userRepository;
-
-    @BeforeEach
-    public void init() {
-        MockitoAnnotations.openMocks(this);
+    @ParameterizedTest
+    @DisplayName("Deve lançar UserException para e-mails inválidos")
+    @ValueSource(strings = {
+            "",                 
+            "   ",              
+            "teste@",           
+            "teste@gmail",      
+            "teste@hotmail.com" 
+    })
+    void deveLancarExcecaoParaEmailInvalido(String email) {
+        UserException ex = assertThrows(UserException.class, () -> UserException.invalidEmail(email));
+        assertTrue(ex.getMessage().contains("E-mail"));
     }
 
-    @Test
-    @DisplayName("Teste, criando um usuário ")
-    public void testCreateUser() {
+    @ParameterizedTest
+    @DisplayName("Não deve lançar exceção para e-mails válidos do Gmail")
+    @ValueSource(strings = {
+            "teste@gmail.com",
+            "usuario123@gmail.com",
+            "user.name@gmail.com"
+    })
+    void naoDeveLancarExcecaoParaEmailValido(String email) {
+        assertDoesNotThrow(() -> UserException.invalidEmail(email));
+    }
 
-        UserModel user = new UserModel("Gustavo", "gustavo@gmail.com ", "1234", LocalDate.parse("2000-01-01"));
+    @ParameterizedTest
+    @DisplayName("Deve lançar UserException para senhas inválidas")
+    @ValueSource(strings = {
+            "",                
+            "abc",             
+            "abcdefghi",       
+            "abcdefghi1",      
+            "ABCDEFGH1",       
+            "Abcdefghi"        
+    })
+    void deveLancarExcecaoParaSenhaInvalida(String senha) {
+        assertThrows(UserException.class, () -> UserException.invalidPassword(senha));
+    }
 
-        when(userRepository.save(user)).thenReturn(user); 
+    @ParameterizedTest
+    @DisplayName("Não deve lançar exceção para senhas válidas")
+    @ValueSource(strings = {
+            "Abcdefg1",
+            "SenhaForte123",
+            "JavaRocks9"
+    })
+    void naoDeveLancarExcecaoParaSenhaValida(String senha) {
+        assertDoesNotThrow(() -> UserException.invalidPassword(senha));
+    }
 
-        UserModel created = userService.createUser(user);
+    static Stream<Arguments> datasInvalidas() {
+        return Stream.of(
+                Arguments.of(null, "A data é obrigatória"),
+                Arguments.of(LocalDate.now().plusDays(1), "no futuro"),
+                Arguments.of(LocalDate.now().minusYears(17), "18 anos")
+        );
+    }
 
-        assertEquals(user.getNome(), created.getNome());
+    @ParameterizedTest
+    @DisplayName("Deve lançar UserException para datas de nascimento inválidas")
+    @MethodSource("datasInvalidas")
+    void deveLancarExcecaoParaDataInvalida(LocalDate data, String mensagemEsperada) {
+        UserException ex = assertThrows(UserException.class, () -> UserException.invalidBirthDate(data));
+        assertTrue(ex.getMessage().contains(mensagemEsperada));
+    }
+
+    @ParameterizedTest
+    @DisplayName("Não deve lançar exceção para datas de nascimento válidas")
+    @ValueSource(strings = {
+            "2000-01-01",
+            "1990-05-15",
+            "1985-12-31"
+    })
+    void naoDeveLancarExcecaoParaDataValida(String data) {
+        LocalDate nascimento = LocalDate.parse(data);
+        assertDoesNotThrow(() -> UserException.invalidBirthDate(nascimento));
     }
 
 }
